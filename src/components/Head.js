4b6abcc7f44f271a21102a5c58310efeb8cf3102
rithgan <rithikgandhi1989@gmail.com>
2021-10-03 18:11:54 +0530

    favicon files added

---
+++ src/components/Head.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const Head = () => {
+	return (
+		<>
+			<Typography variant="h4" sx={{ textAlign: 'center' }}>
+				Bitfarms is Yield Farming protocol for Decentralized Finance
+				Application.
+			</Typography>
+			<Typography variant="h4" sx={{ textAlign: 'center' }}>
+				Connecting all the DeFi features into a yield farming ecosystem
+			</Typography>
+		</>
+	)
+}
+export default Head
