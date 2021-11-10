a89bd27159b99ed8b3fccebba4cf63d7b918f559
rithgan <rithikgandhi1989@gmail.com>
2021-11-10 16:27:15 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Link from './components/Link'
 import Affiliate from './components/Affiliate'
 import Footer from './components/Footer'
 import { Box, Grid } from '@mui/material'
@@ -8,6 +9,7 @@ import { styled } from '@mui/material/styles'
 import BNB from './containers/BNB'
 import BFM from './containers/BFM'
 import BUSD from './containers/BUSD'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
@@ -16,8 +18,19 @@ export default function App() {
       <Box sx={{ flexGrow: 1, background: 'rgb(26, 27, 32)' }}>
         <section className="main-section">
           <div className="container">
+            <Grid
+              item
+              xs={12}
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            >
+              <Link />
+            </Grid>
             <Grid container spacing={4}>
-              <BNB />
+              <Routes>
+                <Route path="/" element={<BNB />} />
+                <Route path="/bfm" exact element={<BFM />} />
+                <Route path="/busd" exact element={<BUSD />} />
+              </Routes>
 
               {/* Affiliate link */}
               <Grid item xs={12} sm={12} md={8}>
---
+++ src/components/Link.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Link = () => {
+	return (
+		<>
+			<a href="/" className="link">
+				BNB |
+			</a>
+			<a href="/busd" className="link">
+				BUSD |
+			</a>
+			<a href="/bfm" className="link">
+				BFM |
+			</a>
+		</>
+	)
+}
+
+export default Link
